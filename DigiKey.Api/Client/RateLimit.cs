dacaf5235158920193bf0aa85c611515128f0977
Mark <markh@rris.com.au>
2022-02-06 05:49:31 +0000

    Add millisecond conversion for rate limit for convenience

--- DigiKey.Api/Client/RateLimit.cs
+++ DigiKey.Api/Client/RateLimit.cs
@@ -19,6 +19,10 @@ namespace DigiKey.Api.Client
         /// </summary>
         public int RetryAfter { get; set; }
         /// <summary>
+        /// The number of milliseconds until you can retry the request
+        /// </summary>
+        public int RetryAfterMilliseconds {  get { return RetryAfter * 1000;  } }
+        /// <summary>
         /// The maximum number of requests allowed for the API, number per minute
         /// </summary>
         public int Limit { get; set; }
@@ -31,7 +35,11 @@ namespace DigiKey.Api.Client
         /// </summary>
         public int Reset { get; set; }
         /// <summary>
-        /// The time when the burst limit window resets, GMT
+        /// The number of milliseconds until the burst limit window resets
+        /// </summary>
+        public int ResetMilliseconds { get { return Reset * 1000; } }
+        /// <summary>
+        /// The time when the burst limit window resets
         /// </summary>
         public DateTime ResetTime { get; set; }
     }
